02dfe66969588f0b8533f9780458fd7021a7d186
TWL-PC\todayswl <necromanser89@gmail.com>
2018-01-31 13:53:21 +0300

    Restruct project. Add folder for tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='codes_viewer',
-    packages=['src'],
+    packages=['viewer'],
     include_package_data=True,
     install_requires=[
         'pyexcel',
